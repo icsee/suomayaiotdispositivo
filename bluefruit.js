af1c9c32e853566031f9d61f1500ff2b8cc97006
juanamezquita <t.amezquita.juancarlos@gmail.com>
2020-08-11 16:23:54 -0500

    Se adicion codigo de prueba bluefruit

---
+++ bluefruit.js
@@ -0,0 +1,48 @@
+var Feather = require("feather-ble");
+
+var possible_feather = some_noble_peripheral_instance;
+ 
+// Check is peripheral is an adafruit feather
+if (new Feather().isFeather(possible_feather)) {
+  // possible_feather is an adafruit feather device
+  
+  // Create settings
+  var instanceSettings = {
+    peripheral: possible_feather,     // REQUIRED: A Noble Peripheral Instance to use
+    verbose: Bool,                    // OPTIONAL: If instance should print out logs to console (default FALSE)
+    rssi: Bool,                       // OPTIONAL: If instance should request/trigger RSSI updates (default FALSE)
+    rssi_update_rate: Int             // OPTIONAL: Rate (in ms) at which RSSI updates should be requested/triggered (default 5000)
+  };
+  
+  // Create instance
+  var feather = new Feather(instanceSettings);
+  
+  // Add event listeners
+  feather.on("ready", function(err){
+    if (!err) {
+      // feather is connected and ready
+      
+      // Can send strings over to adafruit device
+      feather.sendMessage("Hello World!");
+      feather.sendMessage("This is a really long string that works just as well :)");
+    }
+  });
+  
+  feather.on("message", function(msg){
+    // Message recieved from adafruit device
+  });
+  
+  feather.on("rssi", function(err, rssi){
+    if (!err) {
+      // RSSI was updated
+    }
+  });
+  
+  feather.on("disconnect", function(){
+    // feather was disconnected
+  });
+  
+  // Start feather process
+  feather.setup();
+ 
+}
